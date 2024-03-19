0b3d0d27548dbe236fe351532e87cc13d25e6d96
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-03-19 10:52:15 +0100

    Import a new files

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello Alex!"));
+console.log(chalk.green("Hello PJ!"));
+console.log(chalk.yellow("Hello JADE!"));
+console.log(chalk.red("Hello ALICE!"));
